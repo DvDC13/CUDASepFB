e87d1559a9860378517ce1456e552a1627875324
DvDC13 <davidchemaly1@gmail.com>
2024-10-29 11:45:06 +0100

    feat(gpu) : added gpu architecture

---
+++ gpu/choquet.cu
@@ -0,0 +1,5 @@
+//
+// Created by david on 29/10/24.
+//
+
+#include "choquet.cuh"
---
+++ gpu/choquet.cuh
@@ -0,0 +1,9 @@
+//
+// Created by david on 29/10/24.
+//
+
+#ifndef COMPUTATION_CUH
+#define COMPUTATION_CUH
+
+
+#endif //COMPUTATION_CUH
